b6729897a10d2a4919663a2f1f115a008f54339f
Diego Aravena <diego.aravena02@inacapmail.cl>
2023-09-21 19:56:10 -0300

    Added basic navigation

--- app/src/main/java/com/example/morevalidations/ConfirmationActivity.java
+++ app/src/main/java/com/example/morevalidations/ConfirmationActivity.java
@@ -3,6 +3,7 @@ package com.example.morevalidations;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class ConfirmationActivity extends AppCompatActivity {
 
@@ -10,5 +11,9 @@ public class ConfirmationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_confirmation);
+    }
+
+    public void back(View view) {
+        finish();
     }
 }
